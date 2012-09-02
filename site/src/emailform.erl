ee23e70046a4a3bdbb079ac30a5b35646274d5b2
João Lopes <joao@spotify.com>
2012-09-02 20:39:15 +0200

    email form is now calling the erlyemail_scheduler

--- site/src/emailform.erl
+++ site/src/emailform.erl
@@ -1,17 +1,19 @@
 %% -*- mode: nitrogen -*-
 -module (emailform).
--compile(export_all).
+
 -include_lib("nitrogen_core/include/wf.hrl").
 -include("records.hrl").
 
+-compile(export_all).
+
 main() -> #template { file="./site/templates/bare.html" }.
 
-title() -> "Hello from emailform.erl!".
+title() -> "Schedule email".
 
 body() ->
     Body = [
         #panel { style="margin: 50px 100px;", body=[
-            #span { text="Hello from emailform.erl!" },
+            #h2 { text="Schedule email" },
 
             #p{},
             #label{ text="to:" },
@@ -24,8 +26,6 @@ body() ->
             #textbox{ id=subjectTextBox },
             #label{ text="body:" },
             #textarea{ id=bodyTextBox },
-            #label{ text="attachments:" },
-            % TODO: upload multiple files and show a list
             #label{ text="send date (yy-mm-dd):" },
             #datepicker_textbox{ id=dueDateTextBox, options=[
                 {dateFormat, "yy-mm-dd"},
@@ -36,6 +36,16 @@ body() ->
             ]},
             #label{ text="time (hh:mm):" },
             #textbox{ id=dueTimeTextBox },
+            #table { style="width: 100%;", rows=[
+                #tablerow { cells=[
+                    #tableheader { style="width: 33%;", text="attachments" }
+                ]},
+                #tablerow { cells=[
+                    #tablecell { id=attachmentsBottomCell }
+                ]}
+            ]},
+            #upload { tag=attachmentBtnClick, show_button=false },
+
             #br{},
             #button{ id=submitBtn, text="Submit", postback=submitBtnClick },
 
@@ -43,10 +53,53 @@ body() ->
             #panel { id=placeholder }
         ]}
     ],
-    wf:wire(submitBtn, toTextBox, #validate { validators=[
-        #is_required { text="Required." }
-    ]}),
-    Body.
+    Body
+.
+
+datetime_from_str(Date, Time) ->
+    {match, [Year, Month, Day]} = re:run(Date, "(\\d\\d\\d\\d)-(\\d\\d)-(\\d\\d)", [{capture, all_but_first, list}]),
+    {match, [Hour, Minute]} = re:run(Time, "(\\d\\d):(\\d\\d)", [{capture, all_but_first, list}]),
+    NDate = {list_to_integer(Year), list_to_integer(Month), list_to_integer(Day)},
+    NTime = {list_to_integer(Hour), list_to_integer(Minute)},
+    {NDate, NTime}
+.
+
+start_upload_event(attachmentBtnClick) ->
+    ok
+.
+
+finish_upload_event(_Tag, undefined, _, _) ->
+    wf:flash("Please select a file."),
+    ok
+;
+finish_upload_event(_Tag, FileName, LocalFileData, _Node) ->
+    FileSize = filelib:file_size(LocalFileData),
+    {ok, Binary} = file:read_file(LocalFileData),
+    file:delete(LocalFileData),
+    wf:insert_bottom(attachmentsBottomCell, #panel {
+        % TODO: add a delete attachment
+        % TODO: add line with sum of attachments size
+        body=wf:f("~s (~p bytes)", [FileName, FileSize]),
+        actions=#show { effect=pulsate, options=[{times, 1}] }
+    }),
+    % TODO: store the LocalFileData somewhere to send with the email
+    ok
+.
 
 event(submitBtnClick) ->
-    wf:insert_top(placeholder, "<p>You clicked the button!").
+    To = wf:q(toTextBox),
+    Subject = wf:q(subjectTextBox),
+    Body = wf:q(bodyTextBox),
+    Date = wf:q(dueDateTextBox),
+    Time = wf:q(dueTimeTextBox),
+    DateTime = datetime_from_str(Date, Time),
+    [User] = storage:get_user(wf:user()),
+    SmtpConn = esmtp:conn(
+        "erlymail.com",
+        {User#users.smtp_srv, User#users.smtp_prt},
+        {User#users.smtp_usr, User#users.smtp_pwd}
+    ),
+    Message = esmtp_mime:msg(User#users.email, To, Subject, Body),
+    erlymail_scheduler_srv:add(DateTime, SmtpConn, Message),
+    wf:insert_top(placeholder, wf:f("User info: <br />~p", [User]))
+.
